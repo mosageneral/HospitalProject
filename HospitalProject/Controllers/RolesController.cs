11def8d4d2ee9408659e86a520178d577a2487ed
Elmahdy <Elmahdy@DESKTOP-9T3QDAE>
2018-10-30 19:51:23 +0200

    Add project files.

---
+++ HospitalProject/Controllers/RolesController.cs
@@ -0,0 +1,108 @@
+﻿using HospitalProject.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalProject.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList ());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.Roles.Add(role);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, IdentityRole role)
+        {
+            try
+            {
+
+                // TODO: Add delete logic here
+                var findrole = db.Roles.Find(id);
+                db.Roles.Remove(findrole);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ex = ex;
+                return View();
+            }
+        }
+    }
+}
